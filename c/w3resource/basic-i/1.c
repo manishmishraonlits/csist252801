244bcda87880ab99a6a320cf9a5cad79d51b68ae
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2026-02-05 14:07:33 +0530

    20260205

---
+++ c/w3resource/basic-i/1.c
@@ -0,0 +1,18 @@
+/*
+1. Print name, DOB, and mobile number
+
+Write a C program to print your name, date of birth, and mobile number.
+Expected Output:
+
+Name   : Alexandra Abramov
+DOB    : July 14, 1975
+Mobile : 99-9999999999
+*/
+#include <stdio.h>
+int main(void)
+{
+    printf("Name\t: Alexandra Abramov\n");
+    printf("DOB\t: July 14, 1975\n");
+    printf("Mobile\t: 99-9999999999\n");
+    return 0;
+}
