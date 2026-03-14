52e930ce41572371ed00244e1254a95fb99dbc3e
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2026-03-14 13:25:29 +0530

    Demonstrate some programs on if-else Fixes #1

--- c/decision/even-odd.c
+++ c/decision/even-odd.c
@@ -1,6 +1,6 @@
 /*
 WAP to read an integer and find whether it is even or odd.
-Date: 19/02/2026
+Date: 11/03/2026
 */
 
 #include <stdio.h>
@@ -8,4 +8,17 @@ Date: 19/02/2026
 int main(void)
 {
     int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if (n % 2 == 0)
+    {
+        printf("Even!");
+    }
+    else
+    {
+        printf("Odd");
+    }
+
+    return 0;
 }
