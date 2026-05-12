c32f52e23b4920e4e0dcb176eecbf7f270c86bba
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2026-05-12 11:07:41 +0530

    Work on Strings Fixes #10

---
+++ c/user-defined-function/swap.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void swap(int *a, int *b);
+
+int main(void)
+{
+    int a, b, c;
+
+    printf("Enter the value of a: ");
+    scanf("%d", &a);
+    printf("Enter the value of b: ");
+    scanf("%d", &b);
+    swap(&a, &b);
+    printf("The values of a and b after swapping are %d and %d, respectively.", a, b);
+    return 0;
+}
+
+void swap(int *a, int *b)
+{
+    int c = *a;
+    *a = *b;
+    *b = c;
+}
